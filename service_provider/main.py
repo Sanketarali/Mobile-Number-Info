baa63f46ffdad590e44a029b3ad1f98faa5642be
Sanket Arali <110754364+Sanketarali@users.noreply.github.com>
2023-01-29 21:54:23 +0530

    Update main.py

--- service_provider/main.py
+++ service_provider/main.py
@@ -4,8 +4,16 @@ from phonenumbers import geocoder
 from phonenumbers import carrier
 number='+919740481350'
 phone_number=phonenumbers.parse(number)
+
+# Fetch the country of a mobile number
 print(geocoder.description_for_number(phone_number,'en'))
+
+
+# Fetch the time zone of a mobile number
 print(timezone.time_zones_for_number(phone_number))
+
+
+# Fetch the service provider of a mobile number
 print(carrier.name_for_number(phone_number,'en'))
 
 
